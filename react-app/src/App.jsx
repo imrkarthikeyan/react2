81e4bc6c4b6c10a2606cf8feb8f84c8afeed6e3b
Karthikeyan R <2303737720521034@ksrct.net>
2025-02-10 10:23:28 +0530

    creating id_card using props

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Course from './Course';
 import CourseList from './CourseList';
+import Id_card from './Id_card';
+import rk from './assets/rk.jpg'
 
 function App() {
 
   return (
     <>
+      <Id_card name="KARTHIKEYAN R" dept="B.TECH(IT)" year="2023-2027" id="2023CTUIT050" image={rk} />
+      <Id_card name="VIGNESH" dept="BE(CSE)" year="2023-2027" id="2023CTUCS036" />
       <CourseList/>
     </>
   )
--- react-app/src/Id_card.jsx
+++ react-app/src/Id_card.jsx
@@ -1,8 +1,8 @@
 import ksrlogo from './assets/ksrlogo.png'
-import rk from './assets/rk.jpg'
 import barcode from './assets/barcode.jpg'
+import empty from './assets/emptyprofile.png'
 
-function Id_card(){
+function Id_card(props){
     return(
         <div className="iddesign">
             <img src={ksrlogo} alt="" />
@@ -10,18 +10,26 @@ function Id_card(){
             <h5>COLLEGE OF TECHNOLOGY</h5>
             <p>(Autonomous)</p>
             <h3>Tiruchengode - 637 215</h3>
-            <img id="imgsize" src={rk} alt="" />
+            <img id="imgsize" src={props.image} alt="" />
             <div className="nesteddiv">
-                <h4>KARTHIKEYAN R</h4>
-                <p>B.Tech(IT)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2023-2027</p>
+                <h4>{props.name}</h4>
+                <p>{props.dept}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.year}</p>
             </div>
             <img id='barcodesize' src={barcode} alt="" />
-            <h4>2023CTUIT050S</h4>
+            <h4>{props.id}</h4>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;principle</p>
             <hr id="lineclr"></hr>
             <p>www.ksrct.ac.in</p>
         </div>
     )
+}
+
+Id_card.defaultProps={
+    name : "DEFAULT NAME",
+    dept : "BE(CSE)",
+    year : "2022-2026",
+    id : "2023CTUIT040S",
+    image : empty
 }
 
 export default Id_card
