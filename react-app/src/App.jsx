48835576d78fafc53b12abd0738aa4c58927370b
Karthikeyan R <2303737720521034@ksrct.net>
2025-02-10 10:45:32 +0530

    updated Id_cardList

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
 import Course from './Course';
 import CourseList from './CourseList';
-import Id_card from './Id_card';
-import rk from './assets/rk.jpg'
+import Id_cardList from './Id_cardList';
 
 function App() {
 
   return (
     <>
-      <Id_card name="KARTHIKEYAN R" dept="B.TECH(IT)" year="2023-2027" id="2023CTUIT050" image={rk} />
-      <Id_card name="VIGNESH" dept="BE(CSE)" year="2023-2027" id="2023CTUCS036" />
+      <Id_cardList/>
       <CourseList/>
     </>
   )
---
+++ react-app/src/Id_cardList.jsx
@@ -0,0 +1,40 @@
+
+import Id_card from './Id_card';
+import rk from './assets/rk.jpg'
+
+function Id_cardList(){
+    const idcards=[
+        {
+            name:"KARTHIKEYAN R", 
+            dept:"B.TECH(IT)", 
+            year:"2023-2027", 
+            id:"2023CTUIT050", 
+            image:rk
+        },
+        {
+            name:"SIRAJ DSP", 
+            dept:"B.TECH(CSE)", 
+            year:"2023-2027", 
+            id:"2023CTUIT049", 
+            image:rk
+        },
+        {
+            name:"LEO DOS S", 
+            dept:"BE(CSE)", 
+            year:"2022-2026", 
+            id:"2023CTUCS043",
+        }
+    ]
+
+    const allidcards=idcards.map(
+        (idcard) => <Id_card name={idcard.name} dept={idcard.dept} year={idcard.year} id={idcard.id} image={idcard.image} />
+    )
+
+    return(
+        <>
+            {allidcards}
+        </>
+    )
+}
+
+export default Id_cardList
