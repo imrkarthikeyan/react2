6a906f04e3c9cb2ff64188294522dfa67defcefa
Karthikeyan R <2303737720521034@ksrct.net>
2025-02-11 15:14:19 +0530

    implemented useState hooks

--- react-app/src/Course.jsx
+++ react-app/src/Course.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 
 
 function Course(props){
 
+    //let purchased=false;
+
+    const [purchased, setPurchased] = useState(false);
+
     function BuyCourse(discount){
         console.log(props.name, "purchased with", discount, "% discount");
+        setPurchased(true);
+        console.log(purchased);
     }
 
     return(
@@ -12,6 +19,7 @@ function Course(props){
             <h3>{props.name}</h3>
             <p>{props.price}</p>
             <button onClick={() => BuyCourse(20)} >Buy Now</button>
+            <p>{purchased ? "Already purchased" : "Get it now"}</p>
         </div>
     )
 }
