01e988d81277904102b0a645fbe4927481afcfca
Karthikeyan R <2303737720521034@ksrct.net>
2025-04-03 19:17:57 +0530

    Digital clock using useEffect hook

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 //import App from './App.jsx'
-import UseEffectHook from './UseEffectHook.jsx'
+// import UseEffectHook from './UseEffectHook.jsx'
+import Clock from './Clock.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <UseEffectHook/>
+    {/* <UseEffectHook/> */}
+    <Clock/>
   </StrictMode>,
 )
